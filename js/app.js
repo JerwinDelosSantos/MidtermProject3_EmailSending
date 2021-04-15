01d09ddf8444efc84315672f4dd3df20c76f4cde
Jerwin Delos Santos <delossantosjerwin20@gmail.com>
2021-04-15 14:22:52 +0800

    Finish the Project

--- js/app.js
+++ js/app.js
@@ -6,8 +6,6 @@ const sendBtn = document.getElementById('sendBtn'),
       resetBtn = document.getElementById('resetBtn'),
       sendEmailForm = document.getElementById('email-form');
 
-
-
 //Event Listeners
 
 eventListeners();
@@ -22,11 +20,9 @@ function eventListeners() {
     subject.addEventListener('blur', validateField);
 
     //Send email & reset Btn
+    sendEmailForm.addEventListener('submit', sendEmail);
     resetBtn.addEventListener('click', resetForm);
 }
-
-
-
 
 //Functions
 
@@ -34,6 +30,35 @@ function eventListeners() {
 function appInit() {
     //Disable the send Btn on load
     sendBtn.disabled = true;
+}
+
+function sendEmail(e) {
+    e.preventDefault();
+
+    //Show the Spinner
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display = 'block';
+
+    //show the image
+    const sendEmailImg = document.createElement('img');
+    sendEmailImg.src = 'img/mail.gif';
+    sendEmailImg.style.display = 'block';
+
+    //Hide spinner then show the email image
+    setTimeout(function() {
+        //Hide the spinner
+        spinner.style.display = 'none';
+
+        //show the email image
+        document.querySelector('#loaders').appendChild(sendEmailImg);
+
+        //after 5 seconds hide the emailimage and reset the form
+        setTimeout(function() {
+            sendEmailForm.reset();
+            sendEmailImg.remove();
+        }, 5000);
+
+    }, 3000 );
 }
 
 //Validate the fields
